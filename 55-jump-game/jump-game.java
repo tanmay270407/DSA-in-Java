2cb22652668aa7479d0f7741e0f41a7fe399806f
Tanmay Kumar <nikhil270405@gmail.com>
2025-12-09 19:55:51 +0530

    Time: 2 ms (84.10%) | Memory: 47.4 MB (58.58%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int len = nums.length;
+        if (len <= 1){
+          return true;
+        }   
+            
+        int maxReach = 0;  
+                 
+        for (int i = 0; i < len; i++) {
+            if (i > maxReach) return false;         
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+}
